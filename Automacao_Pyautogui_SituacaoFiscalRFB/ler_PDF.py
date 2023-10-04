9cde19e7ab8fc909df2519194d59bb8233c89352
Catiusci Scheffer <cpcscheffer@outlook.com>
2023-10-04 10:52:38 -0300

    ler dctfweb ok

--- Automacao_Pyautogui_SituacaoFiscalRFB/ler_PDF.py
+++ Automacao_Pyautogui_SituacaoFiscalRFB/ler_PDF.py
@@ -14,12 +14,25 @@ def extrair_informacoes(pdf_file):
         texto = ""
         encontrou_omissao = False  # Variável de controle para rastrear se uma omissão foi encontrada
         cnpj_gravado = False  # Variável de controle para rastrear se o CNPJ foi escrito
-        dctfWeb = 'Omissão de DCTFWeb*'
-        gfip = 'Pendência - Omissão de GFIP'
-        debitos_SIEF = 'Pendência - Débito (SIEF)'
-        parcelamento_SISPAR = 'Pendência - Parcelamento (SISPAR)'
-        inscricao_divida= 'Pendência - Inscrição (Sistema DIVIDA)'
-        exigibilidade_suspensa = 'Inscrição com Exigibilidade Suspensa (SIDA)'
+        omissao_dctfWeb = 'Omissão de DCTFWeb*'
+        omissao_dctf = 'Omissão de DCTF'
+        omissao_pgdas_d = 'Omissão de PGDAS-D'
+        omissao_gfip = 'Pendência - Omissão de GFIP'
+        omissao_defis = 'Omissão de DEFIS'
+        omissao_efd_contribuicoes = 'Omissão de EFD-CONTRIB'
+        
+        pendencia_debito_parcelamento = 'Pendência - Parcelamento (PARCSN/PARCMEI)'
+        pendencia_debitos_SIEF = 'Pendência - Débito (SIEF)'
+        pendencia_processo_SIEF = 'Pendência - Processo Fiscal (SIEF)'
+        pendencia_debito_GFIP = 'Pendência - Divergência GFIP x GPS (AGUIA)'
+        pendencia_parcelamento_SISPAR = 'Pendência - Parcelamento (SISPAR)'
+        pendencia_inscricao_divida= 'Pendência - Inscrição (Sistema DIVIDA)'
+        pendencia_divida_SIDA = 'Pendência - Inscrição (SIDA)'
+        pendencia_debito_SICOB = 'Pendência - Débito (SICOB)'
+        parcelamento_PARCSNPARCMEI_susp = 'Parcelamento com Exigibilidade Suspensa (PARCSN/PARCMEI)'
+        processo_SIEF_susp = 'Processo Fiscal com Exigibilidade Suspensa (SIEF)'
+        inscricao_SISPAR_susp = 'Inscrição com Exigibilidade Suspensa (Sistema DIVIDA)'
+        inscricao_SIDA_susp = 'Inscrição com Exigibilidade Suspensa (SIDA)'
         
         
         omit_found = False  # Variável para rastrear se uma omissão foi encontrada
@@ -46,7 +59,7 @@ def extrair_informacoes(pdf_file):
                 cnpj_gravado = True
 
             # Verifica se a linha contém 'Omissão de DCTFWeb*'
-            if dctfWeb in linha:
+            if omissao_dctfWeb in linha:
                 encontrou_omissao = True
                 omit_found = True  # Define omit_found como True
                 with open(arquivo_saida, 'a', encoding='utf-8') as output_file:
